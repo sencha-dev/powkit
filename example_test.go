9dffd7611ea3167d79605f2a240781767fb4609e
Tug Witt <tugw@pm.me>
2021-08-18 18:10:45 -0400

    updated test for go doc

--- example_test.go
+++ example_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 // ETH example of generating a PoW hash and mix digest
-func ExampleETH() {
+func Example_computeETH() {
 	nonce := uint64(0x956e895d988798e)
 	height := uint64(12965001)
 	hash, err := hex.DecodeString("cf133ce0cccd4ad877d671b310c27f5ce19c28c14455dac45b90171bac5581c7")
@@ -31,7 +31,7 @@ func ExampleETH() {
 }
 
 // ETC example of generating a PoW hash and mix digest
-func ExampleETC() {
+func Example_computeETC() {
 	nonce := uint64(0x9827862e22a92ff1)
 	height := uint64(13344137)
 	hash, err := hex.DecodeString("27eaf677273c9147cd27b99c34b3783243255864a54b169af238750c39b3c167")
@@ -54,7 +54,7 @@ func ExampleETC() {
 }
 
 // RVN example of generating a block hash and mix digest
-func ExampleRVN() {
+func Example_computeRVN() {
 	height := uint64(1888509)
 	nonce := uint64(0xf09b0e1342275f3f)
 	headerHash, err := hex.DecodeString("14f2c18d74d48abe637437458c10ff5283a9a5197e8b5e740a161f4411b97a43")
