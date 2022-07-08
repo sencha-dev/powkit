6be808b0bf67103df13bb42d7c33c4667460b854
Tug Witt <tugw@pm.me>
2022-07-07 17:40:14 -0700

    standardize sipnode and sipblock

--- internal/crypto/siphash.go
+++ internal/crypto/siphash.go
@@ -53,3 +53,66 @@ func (h *SipHasher) Hash24(nonce uint64) {
 	h.SipRound()
 	h.SipRound()
 }
+
+func (h *SipHasher) Hash48(nonce uint64) {
+	h.v3 ^= nonce
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.v0 ^= nonce
+	h.v2 ^= 0xff
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+	h.SipRound()
+}
+
+type SipNodeFunc func(uint64, [4]uint64, uint64, uint64) uint64
+
+func SipNode24(edgeMask uint64, siphashKeys [4]uint64, edge, uorv uint64) uint64 {
+	hasher := NewSipHasher(siphashKeys[0], siphashKeys[1], siphashKeys[2], siphashKeys[3])
+	hasher.Hash24(2*edge + uorv)
+	value := hasher.XorLanes()
+
+	return value & edgeMask
+}
+
+func SipNode24Legacy(edgeMask uint64, siphashKeys [4]uint64, edge, uorv uint64) uint64 {
+	hasher := NewSipHasher(siphashKeys[0], siphashKeys[1], siphashKeys[2], siphashKeys[3])
+	hasher.Hash24(2*edge + uorv)
+
+	value := hasher.XorLanes()
+	value = value<<17 | value>>47
+
+	return value & edgeMask
+}
+
+type SipBlockFunc func([4]uint64, uint64) uint64
+
+func SipBlock48(siphashKeys [4]uint64, edge uint64) uint64 {
+	const edgeBlockBits uint64 = 6
+	const edgeBlockSize uint64 = (1 << edgeBlockBits)
+	const edgeBlockMask uint64 = (edgeBlockSize - 1)
+
+	hasher := NewSipHasher(siphashKeys[0], siphashKeys[1], siphashKeys[2], siphashKeys[3])
+	block := make([]uint64, edgeBlockSize)
+	edge0 := edge & ^edgeBlockMask
+
+	var i uint64
+	for i = 0; i < edgeBlockSize; i++ {
+		hasher.Hash48(edge0 + uint64(i))
+		block[i] = hasher.XorLanes()
+	}
+
+	last := block[edgeBlockMask]
+	for i = 0; i < edgeBlockMask; i++ {
+		block[i] ^= last
+	}
+
+	return block[edge&edgeBlockMask]
+}
